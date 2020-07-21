107e305f7fad1dd43d28274ad75c6135f018f7f2
stephen.dall <sdall@ctic-inc.com>
2020-07-21 09:56:02 -0700

    Fixes based on PR feedback
    Fixed calculation error, unit tests.  Added support for publishing to maven central.

--- src/main/java/com/chesapeaketechnology/unitconversion/FrequencyUnit.java
+++ src/main/java/com/chesapeaketechnology/unitconversion/FrequencyUnit.java
@@ -2,6 +2,8 @@ package com.chesapeaketechnology.unitconversion;
 
 /**
  * Supports frequency unit conversions.  Provides utility methods to convert from one frequency unit type to another.
+ *
+ * @since 0.0.1
  */
 public enum FrequencyUnit
 {
@@ -19,17 +21,17 @@ public enum FrequencyUnit
 
                 public double toKhz(double hz)
                 {
-                    return hz / 1_000;
+                    return hz / ONE_THOUSAND;
                 }
 
                 public double toMhz(double hz)
                 {
-                    return hz / 1_000_000;
+                    return hz / ONE_MILLION;
                 }
 
                 public double toGhz(double hz)
                 {
-                    return hz / 1_000_000_000;
+                    return hz / ONE_BILLION;
                 }
 
                 public double convert(double frequency, FrequencyUnit frequencyUnit)
@@ -46,7 +48,7 @@ public enum FrequencyUnit
 
                 public double toHz(double khz)
                 {
-                    return khz * 1_000;
+                    return khz * ONE_THOUSAND;
                 }
 
                 public double toKhz(double khz)
@@ -56,12 +58,12 @@ public enum FrequencyUnit
 
                 public double toMhz(double khz)
                 {
-                    return khz / 1_000;
+                    return khz / ONE_THOUSAND;
                 }
 
                 public double toGhz(double khz)
                 {
-                    return khz / 1_000_000;
+                    return khz / ONE_MILLION;
                 }
 
                 public double convert(double frequency, FrequencyUnit frequencyUnit)
@@ -78,12 +80,12 @@ public enum FrequencyUnit
 
                 public double toHz(double mhz)
                 {
-                    return mhz * 1_000_000;
+                    return mhz * ONE_MILLION;
                 }
 
                 public double toKhz(double mhz)
                 {
-                    return mhz * 1_000;
+                    return mhz * ONE_THOUSAND;
                 }
 
                 public double toMhz(double mhz)
@@ -93,7 +95,7 @@ public enum FrequencyUnit
 
                 public double toGhz(double mhz)
                 {
-                    return mhz / 1_000;
+                    return mhz / ONE_THOUSAND;
                 }
 
                 public double convert(double frequency, FrequencyUnit frequencyUnit)
@@ -110,17 +112,17 @@ public enum FrequencyUnit
 
                 public double toHz(double ghz)
                 {
-                    return ghz * 1_000_000_000;
+                    return ghz * ONE_BILLION;
                 }
 
                 public double toKhz(double ghz)
                 {
-                    return ghz * 1_000_000;
+                    return ghz * ONE_MILLION;
                 }
 
                 public double toMhz(double ghz)
                 {
-                    return ghz * 1_000;
+                    return ghz * ONE_THOUSAND;
                 }
 
                 public double toGhz(double ghz)
@@ -134,70 +136,57 @@ public enum FrequencyUnit
                 }
             };
 
+    // Constants for conversions
+    private static final int ONE_THOUSAND = 1_000;
+    private static final int ONE_MILLION = 1_000_000;
+    private static final int ONE_BILLION = 1_000_000_000;
+
     /**
-     * Convert a frequency to a rounded Hz value
+     * Convert a frequency to a rounded Hz value.
      *
      * @param frequency the frequency to convert
      * @return The frequency in rounded Hz
      */
-    public long toHzRounded(double frequency)
-    {
-        throw new AbstractMethodError();
-    }
+    public abstract long toHzRounded(double frequency);
 
     /**
-     * Convert a frequency to Hz
+     * Convert a frequency to Hz.
      *
-     * @param frequency the frequency to convert
+     * @param frequency the frequency to convert.  The unit is defined by the FrequencyUnit enum that the method is called from.
      * @return The frequency in Hz
      */
-    public double toHz(double frequency)
-    {
-        throw new AbstractMethodError();
-    }
+    public abstract double toHz(double frequency);
 
     /**
-     * Convert a frequency to kHz
+     * Convert a frequency to kHz.
      *
-     * @param frequency the frequency to convert
+     * @param frequency the frequency to convert.  The unit is defined by the FrequencyUnit enum that the method is called from.
      * @return The frequency in kHz
      */
-    public double toKhz(double frequency)
-    {
-        throw new AbstractMethodError();
-    }
+    public abstract double toKhz(double frequency);
 
     /**
-     * Convert a frequency to MHz
+     * Convert a frequency to MHz.
      *
-     * @param frequency the frequency to convert
+     * @param frequency the frequency to convert.  The unit is defined by the FrequencyUnit enum that the method is called from.
      * @return The frequency in MHz
      */
-    public double toMhz(double frequency)
-    {
-        throw new AbstractMethodError();
-    }
+    public abstract double toMhz(double frequency);
 
     /**
-     * Convert a frequency to GHz
+     * Convert a frequency to GHz.
      *
-     * @param frequency the frequency to convert
+     * @param frequency the frequency to convert.  The unit is defined by the FrequencyUnit enum that the method is called from.
      * @return The frequency in GHz
      */
-    public double toGhz(double frequency)
-    {
-        throw new AbstractMethodError();
-    }
+    public abstract double toGhz(double frequency);
 
     /**
-     * Convert a frequency given a source frequency and units
+     * Convert a frequency given a source frequency and units.
      *
      * @param frequency     the frequency to convert
      * @param frequencyUnit the unit of the frequency
-     * @return The frequency in the units associated with the enum that convert is invoked on
+     * @return The frequency in the units defined by the FrequencyUnit enum that the method is called from
      */
-    public double convert(double frequency, FrequencyUnit frequencyUnit)
-    {
-        throw new AbstractMethodError();
-    }
+    public abstract double convert(double frequency, FrequencyUnit frequencyUnit);
 }
